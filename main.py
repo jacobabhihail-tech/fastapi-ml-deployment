b07f2a6471f8911989dc0bb418c4ecb8e3c6f340
jacobabhihail-tech <jacob.abhihail@gmail.com>
2026-03-23 13:42:33 +0530

    Day 72 : /predict endpoint with query  parameters

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def home():
     return{"messge": "API is working"}
+
+@app.get("/predict")
+def predict(age: int, balance: float):
+    return{
+        "age":age,
+        "balance": balance,
+        "prediction":"churn" if balance < 5000 else "no churn"
+    }
